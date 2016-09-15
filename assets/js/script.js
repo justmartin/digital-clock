3256cb81b1cb2b699438a3887136ff99927b5a4f
justmartin <justin.lamar.martin@gmail.com>
2016-09-15 13:45:13 -0700

    Proper indentation on html and js. Added margin to css to center hex clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,44 @@
 $(document).ready(function() {
 
-		getCurrentTime();
-		getCurrentColor();
+	setInterval(getCurrentTime,1000);
+	setInterval(getCurrentColor,1000);
 
 });
 
+var initializeTime = function() {
+	//declare global variables (used in multiple functions)
+	var date = new Date();
+	hours = date.getHours();
+	minutes = date.getMinutes();
+	seconds = date.getSeconds();
 
-//declare global variables (used in multiple functions)
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-var hex_color = "#" + hours + "" + minutes + "" + seconds;
-
-//adds a zero in front of h/m/s to maintain hex code when single digits
-if (hours <= 9) {
-	hours = "0" + hours;
-}
-if (minutes <= 9) {
-	minutes = "0" + minutes;
-}
-if (seconds <= 9) {
-	seconds = "0" + seconds;
-}
+	//adds a zero in front of h/m/s to maintain hex code when single digits
+	if (hours <= 9) {
+		hours = "0" + hours;
+	};
+	if (minutes <= 9) {
+		minutes = "0" + minutes;
+	};
+	if (seconds <= 9) {
+		seconds = "0" + seconds;
+	};
+};
 
 //sets hex-color div to current time
 var getCurrentTime = function() {
+	initializeTime();
 	//jquery
-	$("#hex-clock")[0].innerText = hex_color;
+	$("#hex-clock")[0].innerText = "#" + hours + "" + minutes + "" + seconds;
 	//javascript
-	//document.getElementById("hex-clock").innerText = hex_colorl
+	//document.getElementById("hex-clock").innerText = hex_color;
 };
 
 //sets body background to current hex color (current time)
 var getCurrentColor = function() {
+	initializeTime();
+	var hex_color = "#" + hours + "" + minutes + "" + seconds;
 	//selects body and changes background color with jQuery
 	$("body").css('background-color', hex_color);
-}
+};
 
 
